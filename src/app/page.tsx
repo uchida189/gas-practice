e2cbce6233f72f22b1867bb8ee3e06a2fbc4ec3b
uchida189 <perukipoo@icloud.com>
2024-11-17 00:52:36 +0900

    [feat]送信先を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,29 +25,17 @@ export default function SendData() {
       console.error('エラーが発生しました:', error);
       alert('送信に失敗しました。');
     }
-    // const response = await fetch('https://script.google.com/macros/s/AKfycbxW1j6tNkfn-fZN_F9h9eKuN_t3b-dLWG82h-gHhSBNLCo8AaJWv3O2uMxzU8Ecwlsp9w/exec', { // GAS の URL
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   mode: 'no-cors',
-    //   body: JSON.stringify(formData),
-    // });
-    
-    // const result = await response.json();
-    // if (result.status === 'success') {
-    //   alert('データを送信しました！');
-    // } else {
-    //   alert('エラーが発生しました: ' + result.message);
-    // }
   };
   
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="名前" onChange={handleChange} required />
-      <input type="email" name="email" placeholder="メール" onChange={handleChange} required />
-      <textarea name="message" placeholder="メッセージ" onChange={handleChange} required />
-      <button type="submit">送信</button>
-    </form>
+    <div>
+      <p>送信先: https://script.google.com/macros/s/AKfycbxW1j6tNkfn-fZN_F9h9eKuN_t3b-dLWG82h-gHhSBNLCo8AaJWv3O2uMxzU8Ecwlsp9w/exec</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="名前" onChange={handleChange} required />
+        <input type="email" name="email" placeholder="メール" onChange={handleChange} required />
+        <textarea name="message" placeholder="メッセージ" onChange={handleChange} required />
+        <button type="submit">送信</button>
+      </form>
+    </div>
   );
 }
